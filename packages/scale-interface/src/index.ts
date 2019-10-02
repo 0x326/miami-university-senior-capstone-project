27a87f27b4ff72bfe0a6b8bfd34e6af165344863
John Meyer <1279355-0x326@users.noreply.gitlab.com>
2019-10-02 19:22:57 -0400

    Parameterize serial device

--- packages/scale-interface/src/index.ts
+++ packages/scale-interface/src/index.ts
@@ -1,5 +1,17 @@
+import yargs from 'yargs'
+
 import {
   open,
 } from './serial'
 
-open('/dev/urandom')
+const {
+  argv,
+} = yargs
+  .option('device', {
+    description: 'Serial device to scale',
+    type: 'string',
+    demandOption: true,
+    default: '/dev/ttyUSB0',
+  })
+
+open(argv.device)
