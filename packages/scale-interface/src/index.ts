dea0c489c9a435f8c2c9d777cf631de3593dabd0
John Meyer <1279355-0x326@users.noreply.gitlab.com>
2019-10-05 15:23:59 -0400

    Expose more serial options to commandline

--- packages/scale-interface/src/index.ts
+++ packages/scale-interface/src/index.ts
@@ -13,5 +13,54 @@ const {
     demandOption: true,
     default: '/dev/ttyUSB0',
   })
+  .option('baud-rate', {
+    description: 'Device baud rate',
+    type: 'number',
+    demandOption: true,
+    default: 9600,
+    choices: [
+      1200,
+      2400,
+      4800,
+      9600,
+    ],
+  })
+  .option('data-bits', {
+    description: 'Number of device data bits',
+    type: 'number',
+    demandOption: true,
+    default: 7,
+    choices: [
+      7,
+      8,
+    ],
+  })
+  .option('bit-parity', {
+    description: 'Parity used by device',
+    type: 'string',
+    demandOption: true,
+    default: 'even',
+    choices: [
+      'none',
+      'even',
+      'odd',
+    ],
+  })
 
-open(argv.device)
+const {
+  device,
+  'baud-rate': baudRate,
+  'data-bits': dataBits,
+  'bit-parity': bitParity,
+} = argv as {
+  device: string;
+  'baud-rate': 1200 | 2400 | 4800 | 9600;
+  'data-bits': 7 | 8;
+  'bit-parity': 'none' | 'even' | 'odd';
+}
+
+open(device, {
+  baudRate,
+  dataBits,
+  parity: bitParity,
+})
