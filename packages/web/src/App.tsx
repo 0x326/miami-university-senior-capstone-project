5aa59f13baeac4b86e9a96fb587051b560fc646f
John Meyer <1279355-0x326@users.noreply.gitlab.com>
2019-10-30 15:28:41 -0400

    Show ExperimentDashboard

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, {
+  useState,
+} from 'react'
 
-import logo from './logo.svg'
+import {
+  List,
+  Map,
+} from 'immutable'
+
+import ExperimentDashboard, {
+  ExperimentData,
+  Cages,
+  CageId,
+} from './ExperimentDashboard'
+
+import {
+  CageData,
+} from './CageSessions'
+
+import {
+  CageSessionData,
+} from './CageSessionTable'
+
+import {
+  BottleType,
+} from './types'
+
 import './App.css'
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-)
+const App: React.FC = () => {
+  const [bottleTypes, setBottleTypes] = useState<List<BottleType>>(List.of('H₂0', 'EtOH'))
+  const [experimentData, setExperimentData] = useState<ExperimentData>(Map((List.of(
+    [1, (List.of(
+      [1, (List.of(
+        ['Before', Map(List.of(
+          ['H₂0', 5],
+          ['EtOH', 10],
+        ))],
+      ) as unknown as CageSessionData)],
+    ) as CageData)],
+  ) as List<[CageId, CageData]>)))
+  const [cages, setCages] = useState<Cages>(List.of(1))
+
+  return (
+    <>
+      <ExperimentDashboard
+        bottleTypes={bottleTypes}
+        experimentData={experimentData}
+        cages={cages}
+      />
+    </>
+  )
+}
 
 export default App
