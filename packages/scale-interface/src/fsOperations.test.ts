044454ea2e50a5ada33bda30cf71d6d66ed8302d
Nichoals DiGennaro <digennnj@miamioh.edu>
2019-11-13 15:08:17 -0500

    Commiting to pull the debugger push from John

--- packages/scale-interface/src/fsOperations.test.ts
+++ packages/scale-interface/src/fsOperations.test.ts
@@ -1,33 +1,75 @@
+// import * as os from 'os'
+
 import ws from 'ws'
 
 import {
-  getRootDir,
+  // getRootDir,
   // listExperiments,
-  // writeExperiment,
+  writeExperiment,
   // Experiment,
   // ExperimentWrapper,
 } from './fsOperations'
 
 const PORT = 8080
+const PATH = '.\\SCALE_INTERFACE_DAT\\active\\'
 
-const wsGetRoot = new ws(`ws://localhost:${PORT}/get-root-dir`)
+// const wsGetRoot = new ws(`ws://localhost:${PORT}/get-root-dir`)
 // const wsListExperiments = new ws(`ws://localhost:${PORT}/list-experiments`)
 // const wsGetExperiment = new ws(`ws://localhost:${PORT}/get-experiment`)
 // const wsListPaths = new ws(`ws://localhost:${PORT}/list-experiment-paths`)
-// const wsWriteExperiment = new ws(`ws://localhost:${PORT}/write-experiment`)
+const wsWriteExperiment = new ws(`ws://localhost:${PORT}/write-experiment`)
 // const wsScaleData = new ws(`ws://localhost:${PORT}/scale-data`)
-
-it('is testing the tester', () => {
-  // wsGetRoot.onmessage = (event) => {
-  //   console.log('==/get-root-dir')
-  //   console.log(event.data)
-  //   console.log('===')
+wsWriteExperiment.addEventListener('message', (event) => {
+  console.log('==/write-experiment')
+  console.log(JSON.parse(event.data as string))
+  console.log('===')
+})
+it('writes an empty experiment', () => {
+  // const exampleExperiment = {
+  //   name: 'Addiction Study 1',
+  //   primaryExperimenter: 'Quinn',
+  //   dateInitialized: new Date(),
+  //   lastUpdated: new Date(),
+  //   isComplete: false,
+  //   totalSessions: 30,
+  //   totalColsBegin: 8,
+  //   totalColsMid: 6,
+  //   totalColsEnd: 4,
+  //   subSessionLabelsBegin: [
+  //     'Cage Weight',
+  //     'Cage',
+  //     ['H20 Weights', ['Before', 'After 30m', 'After 24h']],
+  //     ['20% ETOH Weights', ['Before', 'After 30m', 'After 24h']],
+  //   ],
+  //   subSessionLabelsMid: [
+  //     'Cage',
+  //     ['H20 Weights', ['Before', 'After 24h']],
+  //     ['20% ETOH Weights', ['Before', 'After 24h']],
+  //   ],
+  //   subSessionLabelsEnd: [
+  //     'Cage',
+  //     ['H20 Weights', ['After 24h']],
+  //     ['20% ETOH Weights', ['After 24h']],
+  //   ],
+  //   cages: [
+  //     {
+  //       cageWeight: 259,
+  //       cageLabel: 'Cage 1 (Dummy)',
+  //       sessions: [
+  //         {
+  //           'H20 Weights Begore': 432,
+  //           'H20 Weights After 30m': 430,
+  //           '20% ETOH Weights After 24h': 340,
+  //         },
+  //       ],
+  //     },
+  //   ],
   // }
-  console.log(getRootDir())
+  expect(writeExperiment({ path: PATH, data: '' }).catch()).toThrowError(new Error('==Data sent to valid() is null'))
 })
 
 // describe('Returns any amount of experiments', () => {
-//   const exampleExperiment = new Experiment({
+//   const exampleExperiment = {
 //     name: 'Addiction Study 1',
 //     primaryExperimenter: 'Quinn',
 //     dateInitialized: new Date(),
@@ -66,16 +108,15 @@ it('is testing the tester', () => {
 //         ],
 //       },
 //     ],
-//   })
-
+//   }
 
 
 //   // eslint-disable-next-line no-template-curly-in-string
-//   const path = '/media/${os.userInfo().username}/test_usb'
+//   const path = 'C:/Users/USER/Documents/School Work/Capstone/SCALE_INTERFACE_DAT'
 
 //   // No saved experiments
 //   test('Empty return array', () => {
-//     expect(listExperiments({ path })).resolves.toHaveLength(0)
+//     expect(listExperiments({ path, filter: emptyExperiment })).resolves.toHaveLength(0)
 //   })
 
 //   // One saved experiment
