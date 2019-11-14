49e0b91d83f6f280b3f5ea6ea41f4cbe88cb8ab2
John Meyer <1279355-0x326@users.noreply.gitlab.com>
2019-11-13 22:00:25 -0500

    Revert "Remove tests (to put them on a separate branch)"
    This reverts commit 17e153e2bc2c82dcd2b845d6600f84875154a483.

---
+++ packages/scale-interface/src/fsOperations.test.ts
@@ -0,0 +1,79 @@
+import {
+  listExperiments,
+  writeExperiment,
+  Experiment,
+  ExperimentWrapper,
+} from './fsOperations'
+
+describe('Returns any amount of experiments', () => {
+  const exampleExperiment = new Experiment({
+    name: 'Addiction Study 1',
+    primaryExperimenter: 'Quinn',
+    dateInitialized: new Date(),
+    lastUpdated: new Date(),
+    isComplete: false,
+    totalSessions: 30,
+    totalColsBegin: 8,
+    totalColsMid: 6,
+    totalColsEnd: 4,
+    subSessionLabelsBegin: [
+      'Cage Weight',
+      'Cage',
+      ['H20 Weights', ['Before', 'After 30m', 'After 24h']],
+      ['20% ETOH Weights', ['Before', 'After 30m', 'After 24h']],
+    ],
+    subSessionLabelsMid: [
+      'Cage',
+      ['H20 Weights', ['Before', 'After 24h']],
+      ['20% ETOH Weights', ['Before', 'After 24h']],
+    ],
+    subSessionLabelsEnd: [
+      'Cage',
+      ['H20 Weights', ['After 24h']],
+      ['20% ETOH Weights', ['After 24h']],
+    ],
+    cages: [
+      {
+        cageWeight: 259,
+        cageLabel: 'Cage 1 (Dummy)',
+        sessions: [
+          {
+            'H20 Weights Begore': 432,
+            'H20 Weights After 30m': 430,
+            '20% ETOH Weights After 24h': 340,
+          },
+        ],
+      },
+    ],
+  })
+
+  const path = `/media/${os.userInfo().username}/test_usb`
+
+  // No saved experiments
+  test('Empty return array', () => {
+    expect(listExperiments({ path })).resolves.toHaveLength(0)
+  })
+
+  // One saved experiment
+  test('One element returned', () => {
+    writeExperiment(new ExperimentWrapper(path, exampleExperiment))
+
+    // Genrate comparison array
+    const compareListExperiments = [new ExperimentWrapper(path, exampleExperiment)]
+    expect(listExperiments({ path })).resolves.toBe(compareListExperiments)
+  })
+
+  // Multiple saved experiments
+  test('Multiple elements returned', () => {
+    writeExperiment(new ExperimentWrapper(path, exampleExperiment))
+    writeExperiment(new ExperimentWrapper(path, exampleExperiment))
+
+    // Generate comparison array
+    const compareListExperiments = [
+      new ExperimentWrapper(path, exampleExperiment),
+      new ExperimentWrapper(path, exampleExperiment),
+      new ExperimentWrapper(path, exampleExperiment),
+    ]
+    expect(listExperiments({ path })).resolves.toBe(compareListExperiments)
+  })
+})
---
+++ packages/scale-interface/src/tester.ts
@@ -0,0 +1,99 @@
+import ws from 'ws'
+
+console.log(`
+////////////////////////////
+// API FUNCTIONALITY TEST //
+////////////////////////////
+`)
+
+
+const PORT = 8081
+
+const wsGetRoot = new ws(`ws://localhost:${PORT}/get-root-dir`)
+const wsListExperiments = new ws(`ws://localhost:${PORT}/list-experiments`)
+const wsGetExperiment = new ws(`ws://localhost:${PORT}/get-experiment`)
+const wsListPaths = new ws(`ws://localhost:${PORT}/list-experiment-paths`)
+const wsWriteExperiment = new ws(`ws://localhost:${PORT}/write-experiment`)
+const wsScaleData = new ws(`ws://localhost:${PORT}/scale-data`)
+
+let ROOT_DIR: string | null = null
+
+wsGetRoot.addEventListener('message', (event) => {
+  console.log('==/get-root-dir')
+  console.log(JSON.parse(event.data as string))
+  ROOT_DIR = event.data as string
+  console.log('===')
+})
+
+wsListExperiments.addEventListener('message', (event) => {
+  console.log('==/list-experiments')
+  console.log(JSON.parse(event.data as string))
+  ROOT_DIR = event.data as string
+  console.log('===')
+})
+
+wsGetExperiment.addEventListener('message', (event) => {
+  console.log('==/get-experiment')
+  console.log(JSON.parse(event.data as string))
+  console.log('===')
+})
+
+wsListPaths.addEventListener('message', (event) => {
+  console.log('==/list-paths')
+  console.log(JSON.parse(event.data as string))
+  console.log('===')
+})
+
+wsWriteExperiment.addEventListener('message', (event) => {
+  console.log('==/write-experiment')
+  console.log(JSON.parse(event.data as string))
+  console.log('===')
+})
+
+wsScaleData.addEventListener('message', (event) => {
+  console.log('==/scale-data')
+  console.log(JSON.parse(event.data as string))
+  console.log('===')
+})
+
+wsGetRoot.addEventListener('open', () => {
+  wsGetRoot.send('')
+})
+
+wsGetRoot.addEventListener('open', () => {
+  wsGetRoot.send('')
+})
+
+wsListExperiments.addEventListener('open', () => {
+  wsListExperiments.send(JSON.stringify({
+    path: '/media/root/test_usb/SCALE_INTERFACE_DAT/active',
+    filter: {
+      primaryExperimenter: 'NotQuinn',
+    },
+  }))
+})
+
+wsGetExperiment.addEventListener('open', () => {
+  wsGetExperiment.send('/media/root/test_usb/SCALE_INTERFACE_DAT/active/addiction study 14_1590000000000_notquinn')
+})
+
+wsListPaths.addEventListener('open', () => {
+  wsListPaths.send(JSON.stringify({
+    path: '/media/root/test_usb/SCALE_INTERFACE_DAT/active/',
+  }))
+})
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// valid name:                                                                                //
+// <experimentName>_ < dateInitialized > _ < primaryExperimenter >                            //
+//                                                                                            //
+// You could also test experiment format validation with this method. Just give it different  //
+// types of invalid experiment data.                                                          //
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+wsWriteExperiment.addEventListener('open', () => {
+  wsWriteExperiment.send(JSON.stringify({
+    path: '/media/root/test_usb/SCALE_INTERFACE_DAT/active/test study 99_1571826295869_quinn',
+    data: JSON.parse('{"name":"Addiction Study 12","primaryExperimenter":"Quinn","dateInitialized":1572730420004,"lastUpdated":1572730420004,"isComplete":false,"totalSessions":30,"totalColsBegin":8,"totalColsMid":6,"totalColsEnd":4,"subSessionLabelsBegin":["Cage Weight","Cage",["H20 Weights",["Before","After 30m","After 24h"]],["20% ETOH Weights",["Before","After 30m","After 24h"]]],"subSessionLabelsMid":["Cage",["H20 Weights",["Before","After 24h"]],["20% ETOH Weights",["Before","After 24h"]]],"subSessionLabelsEnd":["Cage",["H20 Weights",["After 24h"]],["20% ETOH Weights",["After 24h"]]],"cages":[{"cageWeight":259,"cageLabel":"Cage 1 (Dummy)","sessions":[{"H20 Weights Before":1,"H20 Weights After 30m":2,"H20 Weights After 24h":3,"20% ETOH Weights Before":1,"20% ETOH Weights After 20m":2,"20% ETOH Weights After 24h":3}]}]}'),
+  }))
+})
