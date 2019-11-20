d643bf68b9ce291b4014a12e4a0adb380f79e0c2
John Meyer <1279355-0x326@users.noreply.gitlab.com>
2019-11-19 20:57:31 -0500

    Organize cages by rack

--- packages/web/src/routes/experiment-dashboard/ExperimentDashboard.tsx
+++ packages/web/src/routes/experiment-dashboard/ExperimentDashboard.tsx
@@ -17,6 +17,15 @@ import {
 
 import '@material/top-app-bar/dist/mdc.top-app-bar.css'
 
+
+import {
+  CollapsibleList,
+  SimpleListItem,
+} from '@rmwc/list'
+
+import '@material/list/dist/mdc.list.css'
+import '@rmwc/list/collapsible-list.css'
+
 import {
   BottleType,
 } from '../../types'
@@ -25,24 +34,44 @@ import CageSessions, {
   CageData,
 } from './CageSessions'
 
+export type RackId = number
 export type CageId = number
-export interface ExperimentData extends Map<CageId, CageData> {}
-export interface Cages extends List<CageId> {}
+export interface ExperimentData extends Map<RackId, Map<CageId, CageData>> {}
+export interface CageDisplayOrder extends Map<RackId, List<CageId>> {}
+export interface RackDisplayOrder extends List<RackId> {}
 
 interface Props {
   onDrawerOpen: () => void;
   bottleTypes: List<BottleType>;
   experimentData: ExperimentData;
-  cages: Cages;
+  cageDisplayOrders: CageDisplayOrder;
+  rackDisplayOrder: RackDisplayOrder;
 }
 
 function ExperimentDashboard(props: Props): JSX.Element {
   const {
     onDrawerOpen,
     bottleTypes,
-    cages,
+    cageDisplayOrders,
+    rackDisplayOrder,
     experimentData,
   } = props
+
+  const displayableExperimentData:
+    List<Readonly<[List<Readonly<[CageData, CageId]>>, RackId]>> = (rackDisplayOrder
+      .map((rackId) => cageDisplayOrders.get(rackId))
+      .zip(rackDisplayOrder)
+      .filter(([cageDisplayOrder, rackId]) => cageDisplayOrder !== undefined
+        && experimentData.has(rackId)) as List<[List<CageId>, RackId]>)
+      .map(([cageDisplayOrder, rackId]) => [
+        (cageDisplayOrder
+          .map((cageId) => [
+            (experimentData.get(rackId) as Map<CageId, CageData>).get(cageId),
+            cageId,
+          ])
+          .filterNot(([cageData]) => cageData === undefined) as List<[CageData, CageId]>),
+        rackId,
+      ])
 
   return (
     <>
@@ -58,16 +87,27 @@ function ExperimentDashboard(props: Props): JSX.Element {
         </TopAppBarRow>
       </TopAppBar>
       <TopAppBarFixedAdjust />
-      {cages
-        .filter((cageId) => experimentData.has(cageId))
-        .map((cageId) => (
-          <CageSessions
-            key={cageId}
-            cageNumber={cageId}
-            bottleTypes={bottleTypes}
-            cageData={experimentData.get(cageId) as CageData}
-          />
-        ))}
+      {displayableExperimentData.map(([rackData, rackId]) => (
+        <>
+          <CollapsibleList
+            handle={(
+              <SimpleListItem
+                text={`Rack ${rackId}`}
+                metaIcon="chevron_right"
+              />
+            )}
+          >
+            {rackData.map(([cageData, cageId]) => (
+              <CageSessions
+                key={cageId}
+                cageNumber={cageId}
+                bottleTypes={bottleTypes}
+                cageData={cageData}
+              />
+            ))}
+          </CollapsibleList>
+        </>
+      ))}
     </>
   )
 }
