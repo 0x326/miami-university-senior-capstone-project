929309d448ca49cef422c1cc2ce576376587fb11
John Meyer <1279355-0x326@users.noreply.gitlab.com>
2020-02-21 11:28:15 -0500

    Remove unused variables

--- packages/web/src/routes/experiments/record/ExperimentMetadataView.tsx
+++ packages/web/src/routes/experiments/record/ExperimentMetadataView.tsx
@@ -1,10 +1,7 @@
-import React, {
-  useState,
-} from 'react'
+import React from 'react'
 
 import {
   TopAppBar,
-  TopAppBarActionItem,
   TopAppBarFixedAdjust,
   TopAppBarNavigationIcon,
   TopAppBarRow,
