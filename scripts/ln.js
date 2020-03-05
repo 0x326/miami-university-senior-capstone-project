eb946f281857c77df565db635b31f38e9bb5e232
digennnj <digennnj@miamioh.edu>
2020-03-05 16:36:40 -0500

    Add dependency to peer-package

---
+++ scripts/ln.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+if (process.argv.length !== 4) {
+  console.error('Usage: TARGET PATH')
+  process.exit(1)
+}
+
+const [
+  node,
+  scriptPath,
+  target,
+  linkName,
+] = process.argv
+
+const absoluteTarget = path.resolve(target)
+const absoluteLinkName = path.resolve(linkName)
+
+fs.symlinkSync(absoluteTarget, absoluteLinkName, 'junction')
